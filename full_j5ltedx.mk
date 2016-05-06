c3b768c49b6bba5f862a6c4d527eb5fe6127097e
Nick Verse <nikoskalomiris@gmail.com>
2016-05-06 15:46:24 +0300

    Update full_j5ltedx.mk

--- full_j5ltedx.mk
+++ full_j5ltedx.mk
@@ -20,6 +20,6 @@ $(call inherit-product, device/samsung/j5ltedx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_j5ltedx
-PRODUCT_DEVICE := j5ltedx
+PRODUCT_DEVICE := j5lte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
